3f342e903f73163470ad24d7b09880493d342a58
SMITH07HD <134985521+SMITH07HD@users.noreply.github.com>
2024-06-09 14:49:59 -0400

    ultimo commit

--- index.js
+++ index.js
@@ -5,3 +5,13 @@ function holaMundo() {
 console.log(holaMundo());
  
 module.exports = holaMundo;
+
+
+function suma(a,b) {
+
+   let resultado = a + b 
+
+}
+
+console.log(suma(1 + 2));
+module.exports = suma;
